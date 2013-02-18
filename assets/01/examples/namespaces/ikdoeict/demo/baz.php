e2ff7386769cd756f5eed93c654326cad4f78f15
Bramus <bramus@bram.us>
2013-02-18 15:27:21 +0100

    01: Namespaces Demo

---
+++ assets/01/examples/namespaces/ikdoeict/demo/baz.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ikdoeict\Demo;
+
+class Baz extends \Ikdoeict\DemoBase {
+
+	public function __construct($what) {
+		parent::__construct($what);
+	}
+
+}
---
+++ assets/01/examples/namespaces/ikdoeict/demo/foo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ikdoeict\Demo;
+
+class Foo extends \Ikdoeict\DemoBase {
+
+	public function __construct($what) {
+		parent::__construct($what);
+	}
+
+}
