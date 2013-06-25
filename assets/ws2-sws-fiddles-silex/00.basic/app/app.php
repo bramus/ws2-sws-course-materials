f2a65e6f94df7ad1f2c2159262369cfff178be2a
Bramus <bramus@bram.us>
2013-06-25 15:54:54 +0200

    Remove Silex Fiddles (wrong casing)

--- assets/ws2-sws-fiddles-silex/00.basic/app/app.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Bootstrap
-require __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
-
-// Basic Routing
-$app->get('/', function(Silex\Application $app) {
-	return 'ohai';
-});
--- assets/ws2-sws-fiddles-silex/02.olleh/web/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// Require the app and run it
-require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'app.php';
-$app->run();
