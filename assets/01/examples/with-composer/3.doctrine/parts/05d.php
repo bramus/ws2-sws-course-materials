387a1fd5c8f9ea12aa610cf0cda1ca73a6e335d8
Bramus <bramus@bram.us>
2015-02-06 16:41:32 +0100

    MySQL Connections: change from utf8 to utf8mb4

--- assets/01/examples/with-composer/3.doctrine/parts/05d.php
+++ assets/01/examples/with-composer/3.doctrine/parts/05d.php
@@ -1,5 +1,5 @@
 <?php
 
-$value = $db->fetchColumn('SELECT username FROM users WHERE id = ?', array(1));
+$value = $db->fetchColumn('SELECT username FROM users WHERE id = ?', array(3));
 
 dump($value);
