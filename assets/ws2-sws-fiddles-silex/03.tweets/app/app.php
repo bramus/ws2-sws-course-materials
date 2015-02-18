8b69c6271691d12a0e361f8257bfad5f4fec6c8f
Bramus <bramus@bram.us>
2015-02-18 19:41:00 +0100

    Update fiddles to latest version

--- assets/ws2-sws-fiddles-silex/03.tweets/app/app.php
+++ assets/ws2-sws-fiddles-silex/03.tweets/app/app.php
@@ -14,23 +14,41 @@ $app->error(function (\Exception $e, $code) {
 	}
 });
 
+// Home
 $app->get('/', function(Silex\Application $app) {
+
+	// Redirect to Tweets overview
 	return $app->redirect($app['request']->getBaseUrl() . '/tweets');
+
 });
 
+// Tweets overview
 $app->get('/tweets/', function(Silex\Application $app) use ($tweets) {
+
+	// Loop all tweets and output them in a list
 	$output = '<ul>';
-	foreach ($tweets as $id => $tweet) {
-		$output .= '<li><a href="' . $app['request']->getBaseUrl(). '/tweets/' . $app->escape($id) . '">&para;</a> ' . $app->escape($tweet['text']) . '<br />&mdash; by ' . $app->escape($tweet['author'])  .' <i>' . $app->escape($tweet['created_at']) . '</i></li>';
+	foreach ($tweets as $tweet) {
+		$output .= '<li><a href="' . $app['request']->getBaseUrl(). '/tweets/' . $app->escape($tweet['id']) . '">&para;</a> ' . $app->escape($tweet['text']) . '<br />&mdash; by ' . $app->escape($tweet['author'])  .' <i>' . $app->escape($tweet['created_at']) . '</i></li>';
 	}
 	$output .= '</ul>';
 	return $output;
+
 });
 
+// Tweet detail
 $app->get('/tweets/{id}/', function (Silex\Application $app, $id) use ($tweets) {
-	if (!isset($tweets[$id])) {
+
+	// Make sure the given tweet id exists
+	if (!in_array($id, array_column($tweets, 'id'))) {
 		$app->abort(404, "Tweet $id does not exist");
 	}
-	$tweet = $tweets[$id];
+
+	// Extract the tweet by filtering the tweets array based on the value of the id key
+	$tweet = array_pop(array_filter($tweets, function($tweet) use ($id) { return $tweet['id'] == $id; }));
+
+	// Build and return the HTML representing the tweet
 	return  '<p>On ' . $tweet['created_at'] . ' ' . $app->escape($tweet['author']) . ' tweeted:</p><blockquote>' . $app->escape($tweet['text']) . '</blockquote><p><a href="' . $app['request']->getBaseUrl(). '/tweets">&larr; Back to overview</a></p>';
+
 })->assert('id', '\d+');
+
+// EOF
--- assets/ws2-sws-fiddles-silex/05.tweets-twig/app/bootstrap.php
+++ assets/ws2-sws-fiddles-silex/05.tweets-twig/app/bootstrap.php
@@ -9,6 +9,11 @@ $app = new Silex\Application();
 // App Configuration
 $app['debug'] = true;
 
+// array_column polyfill for those running PHP < 5.5.0
+if (!function_exists('array_column')) {
+    function array_column($arr, $col) { return array_map(function($arr) use ($col) { return $arr[$col]; }, $arr); }
+}
+
 // Use Twig — @note: Be sure to install Twig via Composer first!
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
 	'twig.path' => __DIR__ .  DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'views'
